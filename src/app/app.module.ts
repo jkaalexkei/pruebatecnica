f570ca91ecd60be1215671648d9a9fc5a6a2be5b
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-05-06 11:23:51 -0500

    Prueba Tecnica Angular se agrego el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContenidoComponent,
+   
   ],
   imports: [
     BrowserModule,
