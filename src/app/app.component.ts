37107d55542aeabf88ce6880b91bb3355e06f123
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-05-06 22:05:09 -0500

    se agregaron componentes de la barra de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show:boolean = false;
+  activo:string='';
+
   title = 'myapp';
+
+  empleados = [
+
+    {nombre:'Alexander',edad:38,profesion:'ingeniero'},
+    {nombre:'keidy',edad:20,profesion:'operaria'},
+    {nombre:'pedro',edad:19,profesion:'estudiante'},
+
+
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,38 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContenidoComponent } from './contenido/contenido.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './productos/productos.component';
+import { PerfiluserComponent } from './perfiluser/perfiluser.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
+
+const rutas:Routes = [
+  {path:'',component:InicioComponent,pathMatch: 'full'},
+  {path:'perfil', component:PerfiluserComponent},
+  {path:'productos', component:ProductosComponent},
+  {path:'NotFound404',component:Pagina404Component},
+  {path: '**', redirectTo: 'NotFound404', pathMatch: 'full'},
+  
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContenidoComponent,
+    ProductosComponent,
+    PerfiluserComponent,
+    FooterComponent,
+    InicioComponent,
+    Pagina404Component,
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
